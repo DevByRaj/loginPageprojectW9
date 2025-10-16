ffe7c7e51f31972e756058ebf8d7892ce1437b09
DevByRaj <rajmohan0816@gmail.com>
2025-10-16 12:38:01 +0530

    bug fixed

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.use(
     secret: "raj12raj", 
     resave: false,
     saveUninitialized: false,
-    cookie: {httpOnly: true, maxAge: 60000},
+    cookie: {httpOnly: true, maxAge: 600000},
   })
 );
 
@@ -32,7 +32,7 @@ const USER = [
   
 // Authentication middleware
 function authMiddleware(req, res, next) {
-  if (req.session && req.session.users) {
+  if (req.session && req.session.user) {
     next();
   }
   else {
@@ -42,7 +42,7 @@ function authMiddleware(req, res, next) {
 
 // Routes
 app.get("/", (req, res) => {
-  if (req.session.users) {
+  if (req.session.user) {
     return res.redirect("/home");
   }
   res.render("login", {error: null});
@@ -50,10 +50,10 @@ app.get("/", (req, res) => {
 
 app.post("/login", (req, res) => {
   const {username, password} = req.body;
-  const users = USER.find(user => user.username === username && user.password === password)
+  const users = USER.find(u => u.username === username && u.password === password)
   // if (username === USER.username && password === USER.password) {
   if(users){
-    req.session.users = username;
+    req.session.user = username;
    
     return res.redirect("/home");
   } else {
@@ -64,10 +64,15 @@ app.post("/login", (req, res) => {
 
 app.get("/home", authMiddleware, (req, res) => {
 
-   if (!req.session.users) {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  res.setHeader("Surrogate-Control", "no-store");
+
+   if (!req.session.user) {
     return res.redirect("/");
   }
-   return res.render("home", {username: req.session.users} );
+   return res.render("home", {username: req.session.user} );
    
 });
 
